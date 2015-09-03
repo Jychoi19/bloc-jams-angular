19ec071b2fda9df3a8170bdb04767464c3d35afb
Joon Choi <jychoi19@gmail.com>
2015-09-03 21:00:15 +0900

    Adds Routing and States

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,26 @@
-var blocJams = angular.module('blocJams', []);
+var app = angular.module('blocJams', ['ui.router']);
+
+ myAppModule.config(function($stateProvider, $locationProvider) {
+ 
+      $locationProvider.html5Mode({
+         enabled: true,
+         requireBase: false
+     });
+
+     $stateProvider
+     	.state('landing', {
+        	url: '/landing',
+         	controller: 'Landing.controller',
+         	templateUrl: '/templates/landing.html'
+     	})
+     	.state('collection', {
+        	 url: '/collection',
+         	controller: 'Collection.controller',
+         	templateUrl: '/templates/collection.html'
+     	})
+        .state('album', {
+        	 url: '/album',
+         	controller: 'Album.controller',
+         	templateUrl: '/templates/album.html'
+     	})
+ });
